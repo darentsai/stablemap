fb07ce37cab01c4cc9dcd4f8325fc2ebfe3ab13e
Darren Tsai <darrentsai1222@gmail.com>
2025-10-23 17:06:18 +0800

    Add files via upload

--- R/apc.R
+++ R/apc.R
@@ -1,10 +1,10 @@
 #' Design Matrix of Age-Period-Cohort Model
 #'
-#' Construct the APC design matrix with an age-drift parameterization.
+#' Construct a linear or quadratic APC design matrix with an age-drift parameterization.
 #'
-#' @param A Age
-#' @param P Period
-#' @param C Cohort
+#' @param A Age index
+#' @param P Period index
+#' @param C Cohort index
 #' @param degree 1 for linear APC model; 2 for quadratic APC model
 #'
 #' @return A matrix object
@@ -22,9 +22,10 @@ apc_dmat <- function(A, P, C, degree = 1) {
   p <- unique(P)
   c <- unique(C)
 
-  Apos <- outer(A, a, `==`)
-  Ppos <- outer(P, p, `==`)
-  Cpos <- outer(C, c, `==`)
+  # one-hot dummy matrix
+  Adum <- outer(A, a, `==`)
+  Pdum <- outer(P, p, `==`)
+  Cdum <- outer(C, c, `==`)
 
   A0 <- A - M[1]
   P0 <- P - M[2]
@@ -36,34 +37,37 @@ apc_dmat <- function(A, P, C, degree = 1) {
     C0 <- cbind(C0, C0^2)
   }
 
+  # age curvatures
   Ax <- cbind(1, A0)
   Acoef <- solve(crossprod(Ax), t(Ax))
-  Adm <- (I - Ax %*% Acoef) %*% Apos
+  Acurv <- (I - Ax %*% Acoef) %*% Adum
 
+  # period curvatures
   Px <- cbind(1, P0)
   Pcoef <- solve(crossprod(Px), t(Px))
-  Pdm <- (I - Px %*% Pcoef) %*% Ppos
+  Pcurv <- (I - Px %*% Pcoef) %*% Pdum
 
+  # cohort curvatures
   Cx <- cbind(1, C0)
   Ccoef <- solve(crossprod(Cx), t(Cx))
-  Cdm <- (I - Cx %*% Ccoef) %*% Cpos
+  Ccurv <- (I - Cx %*% Ccoef) %*% Cdum
 
   if(degree == 1) {
 
-    # A-2 age devs, P-2 period devs, C-2 cohort devs
-    dm <- cbind(1, A0, C0,
-                Adm[, -c(1, ncol(Adm))],
-                Pdm[, -c(1, ncol(Pdm))],
-                Cdm[, -c(1, ncol(Cdm))], deparse.level = 0)
+    # A-2 age curvs, P-2 period curvs, C-2 cohort curvs
+    dmat <- cbind(1, A0, C0,
+                  Acurv[, -c(1, ncol(Acurv))],
+                  Pcurv[, -c(1, ncol(Pcurv))],
+                  Ccurv[, -c(1, ncol(Ccurv))], deparse.level = 0)
 
   } else if(degree == 2) {
 
-    # A-3 age devs, P-3 period devs, C-3 cohort devs
-    dm <- cbind(1, A0[, 1], C0[, 1], A0[, 2], P0[, 2], C0[, 2],
-                Adm[, -c(1:2, ncol(Adm))],
-                Pdm[, -c(1:2, ncol(Pdm))],
-                Cdm[, -c(1:2, ncol(Cdm))], deparse.level = 0)
+    # A-3 age curvs, P-3 period curvs, C-3 cohort curvs
+    dmat <- cbind(1, A0[, 1], C0[, 1], A0[, 2], P0[, 2], C0[, 2],
+                  Acurv[, -c(1:2, ncol(Acurv))],
+                  Pcurv[, -c(1:2, ncol(Pcurv))],
+                  Ccurv[, -c(1:2, ncol(Ccurv))], deparse.level = 0)
   }
 
-  return(dm)
+  return(dmat)
 }
